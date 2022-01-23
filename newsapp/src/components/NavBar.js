3218c4f1c9fb748688de302e45ae79464c781b4f
shekhartz <hshekhar0@gmail.com>
2022-01-23 17:56:25 +0530

    news-app added

---
+++ newsapp/src/components/NavBar.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+export class NavBar extends Component {
+  static propTypes = {};
+
+  render() {
+    return (
+    <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">NewsMonkey</a>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item"><a className="nav-link active" aria-current="page" href="/">Home</a></li>
+                        <li className="nav-item"><a className="nav-link" href="/about">About</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>)
+  }
+}
+
+export default NavBar;
