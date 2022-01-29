70cb41a8dbbfb0b222a1c5488beb3b3f856d8ee6
shekhartz <hshekhar0@gmail.com>
2022-01-29 17:42:14 +0530

    updated

--- newsapp/src/components/NavBar.js
+++ newsapp/src/components/NavBar.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export class NavBar extends Component {
   static propTypes = {};
@@ -9,11 +10,17 @@ export class NavBar extends Component {
     <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">NewsMonkey</a>
+                <Link className="navbar-brand" to="/">NewsMonkey</Link>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item"><a className="nav-link active" aria-current="page" href="/">Home</a></li>
-                        <li className="nav-item"><a className="nav-link" href="/about">About</a></li>
+                        <li className="nav-item"><Link className="nav-link" aria-current="page" to="/">Home</Link></li>
+                        <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
+                        <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link></li>
+                        <li className="nav-item"><Link className="nav-link" to="/general">General</Link></li>
+                        <li className="nav-item"><Link className="nav-link" to="/health">Health</Link></li>
+                        <li className="nav-item"><Link className="nav-link" to="/science">Science</Link></li>
+                        <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
+                        <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li>
                     </ul>
                 </div>
             </div>
